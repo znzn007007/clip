7679be97a6c3f69f82c965006fcf02e2234a400b
zhukailu <599103359@qq.com>
2026-01-19 11:13:31 +0800

    feat(dedupe): add module exports

---
+++ src/core/dedupe/index.ts
@@ -0,0 +1,4 @@
+// src/core/dedupe/index.ts
+export { DedupeManager } from './manager.js';
+export { getDedupeKey, getDedupeKeyFromUrl } from './strategy.js';
+export type { ArchiveRecord, ArchiveDatabase, DedupeCheckResult } from './types.js';
